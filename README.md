dd40841345cf6ec4b06c738f7e70810b1e3d9595
Carlos Zeballos <carloszeballos.developer@gmail.com>
2019-10-01 19:23:07 -0400

    Added a PowerShell script for Windows' Firewall

---
+++ README.md
@@ -0,0 +1,9 @@
+## Overwatch Firewall Rule 
+Este script crea una regla en el firewall de Windows que bloquea la coneción a los servidores de Blizzard en Brasil "SAE-1", haciendo que el cliente se conecte a los servidores alojados en Estados Unidos.
+### Como usar
+* Descargue el archivo rule.ps1 o clone el repocitorio a su disco duro con el siguiente comando: 
+	```powershell
+	git clone git@github.com:czeballos/Owerewatch-Firewall-Rule.git.
+	```
+* Abrír una consola de Powershell con permisos de administrador.
+* Ejecute el archivo rule.ps1.
---
+++ rule.ps1
@@ -0,0 +1,46 @@
+<#
+Este script crea una regla en el firewall de Windows que bloquea la coneción 
+a los servidores de Blizzard en Brasil "SAE-1", haciendo que el cliente se 
+conecte a los servidores alojados en Estados Unidos.
+#>
+
+$Hosts =	
+"18.228.0.0/16", 
+"18.229.0.0/16",
+"18.231.0.0/16", 
+"52.67.0.0/16", 
+"52.94.0.0/16",
+"52.95.0.0/16",
+"54.94.0.0/16", 
+"52.207.0.0/16", 
+"54.207.0.0/16", 
+"54.232.0.0/16", 
+"54.233.0.0/16", 
+"177.71.0.0/16"
+
+function CreateRule {
+	$GamePath = Read-Host "Indique la ruta donde está instalado Overwatch [ej. c:\Overwatch\_retail_\Overwatch.exe]"
+	New-NetFirewallRule -DisplayName "Overwatch" `
+		-Direction Outbound `
+		-Program $GamePath `
+		-LocalPort Any `
+		-Protocol Any `
+		-Action Block `
+		-RemoteAddress $Hosts
+}
+
+function RemoveRule {
+	Remove-NetFirewallRule -DisplayName "Overwatch"
+}
+
+
+if ($(Get-NetFirewallRule -DisplayName "Overwatch" -ErrorAction SilentlyContinue)) {
+	RemoveRule
+	CreateRule	
+}
+else {
+	CreateRule
+}
+
+Clear-Host
+Write-Host "La regla fue creada satisfactoriamente."
